b1d966384ce97adf1b1b2e2a7ab26569bf21ee63
JimCampagno <jimcampagno@gmail.com>
2016-08-04 16:25:02 -0400

    Remove Xcode project

--- Pods/Target Support Files/Pods-LoveNotWar/Pods-LoveNotWar-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LoveNotWar : NSObject
-@end
-@implementation PodsDummy_Pods_LoveNotWar
-@end
--- Pods/Target Support Files/Pods-LoveNotWarUITests/Pods-LoveNotWarUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LoveNotWarUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LoveNotWarUITests
-@end
